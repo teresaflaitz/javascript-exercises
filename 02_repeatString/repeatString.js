0abff8428a31a86aa23a329f8610708677fb2fe7
teresaflaitz <33165671+teresaflaitz@users.noreply.github.com>
2024-01-28 13:35:31 -0500

    Solved 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    newString = "";
+    while (num > 0) {
+        newString = newString.concat(string);
+        num--;
+    }
+    return(newString);
 };
 
 // Do not edit below this line
