b96756421bb14d82abf80f0a6a83f130fa49e952
teresaflaitz <33165671+teresaflaitz@users.noreply.github.com>
2024-01-28 16:31:01 -0500

    Solved 03 and 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let textReversed = "";
+    for (let i = text.length - 1; i >= 0; i--) {
+        textReversed += text.at(i);
+    }
+    return textReversed;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...toRemove) {
+    const myNewArray = [];
 
+    for (i in myArray) {
+        let j = 0;
+            while(myArray[i] !== toRemove[j] && j < toRemove.length) {
+                j++;
+            }
+            if (j === toRemove.length) {
+                myNewArray.push(myArray[i]);
+            }
+    }
+    return myNewArray;
 };
 
 // Do not edit below this line
