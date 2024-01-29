350608a4fefa0c32932fe360d4b8dc69da20d58e
teresaflaitz <33165671+teresaflaitz@users.noreply.github.com>
2024-01-29 15:13:12 -0500

    Solved 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(smallInt, bigInt) {
+    if (typeof(smallInt) !== 'number' || typeof(bigInt) !== 'number'){
+        return 'ERROR';
+    }
+    
+    if (smallInt < 0 || bigInt < 0){
+        return 'ERROR';
+    }
 
+    if (smallInt > bigInt) {
+        let tempSmallInt = bigInt;
+        let tempBigInt = smallInt;
+
+        smallInt = tempSmallInt;
+        bigInt = tempBigInt;
+    }
+
+    let sum = 0;
+    while (smallInt <= bigInt) {
+        sum += smallInt;
+        smallInt++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
