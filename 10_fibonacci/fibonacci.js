c804d7712114d6234eb714a9116bdcc4ade37a4a
teresaflaitz <33165671+teresaflaitz@users.noreply.github.com>
2024-02-11 18:11:19 -0500

    Solved 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    
+    if (n < 0) {
+        return "OOPS";
+    }
+    else if (n < 2) {
+        return n;
+    }
+    else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    };
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
