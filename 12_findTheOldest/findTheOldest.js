a58bf1170d1c92849132c3e321b8f9bf6595a3ce
teresaflaitz <33165671+teresaflaitz@users.noreply.github.com>
2024-02-11 18:46:31 -0500

    Solved 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const oldToYoung = arr.sort( function(a, b) {
+        if(!("yearOfDeath" in a)) {
+            return ((b.yearOfDeath - b.yearOfBirth) - ((new Date).getFullYear() - a.yearOfBirth));
+        }
+        else if (!("yearOfDeath" in b)) {
+            return (((new Date).getFullYear() - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth));
+        };
+        return ((b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth));
+    });
+    return oldToYoung[0];
 };
 
 // Do not edit below this line
