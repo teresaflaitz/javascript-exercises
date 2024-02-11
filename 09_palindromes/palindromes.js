f8a44802c90fb5bf7cb12895537cd3400f09bc8f
teresaflaitz <33165671+teresaflaitz@users.noreply.github.com>
2024-02-11 16:39:04 -0500

    Updated solution to 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,17 +1,7 @@
 const palindromes = function (str) {
-    strLower = str.toLowerCase();
-    strAlphaNumer = strLower.replace(/[^0-9a-z]/gi, "");
-    const arr = strAlphaNumer.split("");
-    while (arr.length > 1) {
-        if (arr[0] === arr[arr.length - 1]) {
-            arr.shift();
-            arr.pop();
-        }
-        else {
-            return false;
-        };
-    };
-    return true;
+    const strLower = str.toLowerCase();
+    const strAlphaNumer = strLower.replace(/[^0-9a-z]/gi, "");
+    return strAlphaNumer == strAlphaNumer.split("").reverse().join("");
 };
 
 // Do not edit below this line
